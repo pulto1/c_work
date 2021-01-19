36b3ecb632a5e69f95270eeb334c47c09ff451ab
pulto <1016828439@qq.com>
2021-01-19 11:06:26 +0800

    杨氏三角

--- work-20-10-16/work-20-10-16/test4.c
+++ work-20-10-16/work-20-10-16/test4.c
@@ -4,10 +4,41 @@
 
 //有一个数字矩阵，矩阵的每行从左到右是递增的，矩阵从上到下是递增的，请编写程序在这样的矩阵中查找某个数字是否存在。
 
-
+int check(int arr[3][3], int* p1, int* p2, int k)
+{
+	*p1 = *p1 - 1;
+	*p2 = 0;
+	while (*p1 >= 0 && *p2 < 3)
+	{
+		if (arr[*p1][*p2] == k)
+		{
+			return 1;
+		}
+		else if (arr[*p1][*p2] > k)
+		{
+			(*p1)--;
+		}
+		else if (arr[*p1][*p2] < k)
+		{
+			(*p2)++;
+		}
+	}
+	return 0;
+}
 
 int main()
 {
-
+	int arr[3][3] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+	int x = 3;
+	int y = 3;
+	int ret = check(arr, &x, &y, 2);
+	if (1 == ret)
+	{
+		printf("找到了，下标是：%d %d\n", x, y);
+	}
+	else
+	{
+		printf("找不到\n");
+	}
 	return 0;
 }
